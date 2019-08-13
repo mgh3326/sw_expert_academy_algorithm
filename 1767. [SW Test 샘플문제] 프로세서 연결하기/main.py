9429a434e8a40fa23d95d4933046029fd380c1ec
kwanghyunMoon <mgh3326@naver.com>
2019-08-13 18:45:19 +0900

    맥시노스 시간 초과
    맥시노스 시간 초과

--- 1767. [SW Test 샘플문제] 프로세서 연결하기/main.py
+++ 1767. [SW Test 샘플문제] 프로세서 연결하기/main.py
@@ -1,21 +1,129 @@
-def left_check(point):
-    w, h = point
-    for _width in range(w):
-        if map_list[h][_width] != 0:
-            for __width in range(_width):
-                map_list[h][__width] = 0
+import copy
 
-            map_list[h][w] = 3
+
+def left_check(point, _map_list):
+    h, w = point
+    for _width in range(w):
+        if _map_list[h][_width] != 0:
+            for __width in range(_width):
+                _map_list[h][__width] = 0
+
+            _map_list[h][w] = 3
             return
         else:
-            map_list[h][_width] = 1
-    for _width in range(w):
-        map_list[h][w] = 2
+            _map_list[h][_width] = 1
+    _map_list[h][w] = 2
+    global result
+    result += 1
+    global line
+    line += w
+
+
+def right_check(point, _map_list):
+    h, w = point
+    for _width in range(n - w - 1):
+        if _map_list[h][n - _width - 1] != 0:
+            for __width in range(n - _width - 1):
+                _map_list[h][n - __width - 1] = 0
+            _map_list[h][w] = 3
+            return
+        else:
+            _map_list[h][n - _width - 1] = 1
+    _map_list[h][w] = 2
+    global result
+    result += 1
+    global line
+    line += n - w - 1
+
+
+def upper_check(point, _map_list):
+    h, w = point
+    for _height in range(h):
+        if _map_list[_height][w] != 0:
+            for __height in range(_height):
+                _map_list[__height][w] = 0
+
+            _map_list[h][w] = 3
+            return
+        else:
+            _map_list[_height][w] = 1
+    _map_list[h][w] = 2
+    global result
+    result += 1
+    global line
+    line += h
+
+
+def down_check(point, _map_list):
+    h, w = point
+
+    for _height in range(n - h - 1):
+        if _map_list[n - _height - 1][w] != 0:
+            for __height in range(n - _height - 1):
+                _map_list[__height][w] = 0
+
+            _map_list[h][w] = 3
+            return
+        else:
+            _map_list[n - _height - 1][w] = 1
+    _map_list[h][w] = 2
+    global result
+    result += 1
+    global line
+    line += n - h - 1
+
+
+def dfs(_map_list=None, _saved_list=[]):
+    global result
+    global max_result
+    global line
+    global min_line
+    if len(_saved_list) == 0:
+        if max_result < result:
+            max_result = result
+            min_line = line
+        elif max_result == result:
+            if min_line > line:
+                min_line = line
+        return
+    saved_list_copy = copy.deepcopy(_saved_list)
+    saved_list_copy_pop = saved_list_copy.pop(0)
+    map_list_copy = copy.deepcopy(_map_list)
+    _result_copy = result
+    line_copy = line
+    left_check(saved_list_copy_pop[0], _map_list)
+    dfs(_map_list, saved_list_copy)
+    _map_list = copy.deepcopy(map_list_copy)
+    result = _result_copy
+    line = line_copy
+    right_check(saved_list_copy_pop[0], _map_list)
+    dfs(_map_list, saved_list_copy)
+    _map_list = copy.deepcopy(map_list_copy)
+    result = _result_copy
+    line = line_copy
+    upper_check(saved_list_copy_pop[0], _map_list)
+    dfs(_map_list, saved_list_copy)
+    _map_list = copy.deepcopy(map_list_copy)
+    result = _result_copy
+    line = line_copy
+    down_check(saved_list_copy_pop[0], _map_list)
+    dfs(_map_list, saved_list_copy)
+    _map_list = copy.deepcopy(map_list_copy)
+    result = _result_copy
+    line = line_copy
+    dfs(_map_list, saved_list_copy)
+    _map_list = copy.deepcopy(map_list_copy)
+    result = _result_copy
+    line = line_copy
 
 
 test_case_num = int(input())
+
 for test_case_index in range(test_case_num):
     n = int(input())
+    result = 0
+
+    max_result = 0
     # 입력의 첫 번째 줄은 배열의 행 수입니다.
     map_list = []
     # 입력을 받되 첫줄 혹은 끝줄의 경우 2(전선 연결 완료)로 표시하도록 하자
@@ -23,24 +131,27 @@ for test_case_index in range(test_case_num):
     for i in range(n):
         temp_list = list(map(int, input().split()))
         if i == 0 or i == n - 1:
-            for temp in temp_list:
+            for idx, temp in enumerate(temp_list):
                 if temp == 1:
-                    temp_list[temp_list.index(temp)] = 2
+                    temp_list[idx] = 2
+                    result += 1
+
         else:
-            for temp in temp_list:
-                if temp_list.index(temp) == 0 or temp_list.index(temp) == n - 1:
+            for idx, temp in enumerate(temp_list):
+                if idx == 0 or idx == n - 1:
                     if temp == 1:
-                        temp_list[temp_list.index(temp)] = 2
-                        saved_list.append((i, temp_list.index(temp)))
+                        temp_list[idx] = 2
+                        result += 1
+                        # saved_list.append([(i, idx), True])
                 else:
                     if temp == 1:
-                        saved_list.append([(i, temp_list.index(temp)), False])
+                        saved_list.append([(i, idx), 0])
 
         map_list.append(temp_list)
-    left_check(saved_list[0][0])
-    saved_list[0][1] = True
 
-    print()
-    max_num = max(map_list)
-    max_num_index_list = [i for i, val in enumerate(map_list) if val == max_num]
-    # 와 코드를 이렇게도 짤수 있구나 if 저게 True면 i를 반환하나보다 멋지다리
+    max_result = result
+    line = 0
+    min_line = n * n
+    dfs(map_list, saved_list)
+
+    print(min_line)
---
+++ 1767. [SW Test 샘플문제] 프로세서 연결하기/practice.py
@@ -0,0 +1,9 @@
+def param_test():
+    global a
+    a = a + 1
+
+
+a = 1
+print(a)
+param_test()
+print(a)
