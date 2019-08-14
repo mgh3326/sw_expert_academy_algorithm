bd8dab0ff374ea63e867677f137b06fd6f62414b
kwanghyunMoon <mgh3326@naver.com>
2019-08-14 17:26:25 +0900

    update

--- 모의 SW 역량테스트/5653. [모의 SW 역량테스트] 줄기세포배양/main.py
+++ 모의 SW 역량테스트/5653. [모의 SW 역량테스트] 줄기세포배양/main.py
@@ -7,119 +7,73 @@ def double_list_get_list(_input: list, position: list):
     return _input[position[0]][position[1]]
 
 
-def check_map(dir_idx: int, _peak: list):
+def grow_cells():
     global map_list
     global result
-    global max_result
-    global is_cutting
-    nx, ny = dx[dir_idx], dy[dir_idx]
-    peak_value = map_list[_peak[0]][_peak[1]][0]
-    origin_peak = _peak.copy()
-    _peak[0] = _peak[0] + ny
-    _peak[1] = _peak[1] + nx
-    if 0 <= _peak[1] < n and 0 <= _peak[0] < n:
-        if map_list[_peak[0]][_peak[1]][1] == False:
-            if peak_value > map_list[_peak[0]][_peak[1]][0]:
+    for cell in reversed(cell_list):
 
-                map_list[_peak[0]][_peak[1]][1] = True
-                result += 1
-                if max_result < result:
-                    max_result = result
-                return True
-            else:
-                if is_cutting == False:
-                    var_value = map_list[_peak[0]][_peak[1]][0] - peak_value
-                    for cutting_value in range(var_value + 1, k + 1):
-                        is_cutting = True
-                        map_list[_peak[0]][_peak[1]][0] -= cutting_value
-                        map_list[_peak[0]][_peak[1]][1] = True
+        if len(cell) == 0:
+            continue
+        remove_list = []
+        temp_cell_list = []
+        cell_index = cell_list.index(cell)
+        for idx, c in enumerate(cell):
+
+            c[1] -= 1
+            if c[1] == -1:
+                remove_list.append(idx)
+                for dir_idx in range(4):
+                    nx, ny = dx[dir_idx], dy[dir_idx]
+                    current_h = c[0][0] + ny
+                    current_w = c[0][1] + nx
+                    value = map_list[current_h][current_w]
+                    if value == 0:
+                        map_list[current_h][current_w] = cell_index
+                        temp_cell_list.append(
+                            [
+                                [current_h, current_w], cell_index
+                            ]
+                        )
                         result += 1
-                        if max_result < result:
-                            max_result = result
-                        dfs(_peak)
-                        is_cutting = False
-                        map_list[_peak[0]][_peak[1]][0] += cutting_value
-                        map_list[_peak[0]][_peak[1]][1] = False
-                        result -= 1
-
-
-def dfs(_peak: list):
-    global map_list
-    global result
-    global max_result
-    if not 0 <= _peak[1] < n:
-        return
-    if not 0 <= _peak[0] < n:
-        return
-    for _dir_idx in range(4):
-        return_temp = check_map(dir_idx=_dir_idx, _peak=_peak)
-        if return_temp is not None:
-            dfs(_peak)
-            map_list[_peak[0]][_peak[1]][1] = False
+        for remove in reversed(remove_list):
+            cell.remove(cell[remove])
             result -= 1
-
-        nx, ny = dx[_dir_idx], dy[_dir_idx]
-        _peak[0] = _peak[0] + ny * -1
-        _peak[1] = _peak[1] + nx * -1
-
-
-def breaking_brick(point: list):
-    value = double_list_get_list(map_list, point)
-    removed_list = []
-    for dir_idx in range(len(dx)):
-        temp_removed_list = []
-        nx, ny = dx[dir_idx], dy[dir_idx]
-        for value_index in range(1, value):
-            new_point_h = point[0] + ny * value_index
-            new_point_w = point[1] + nx * value_index
-            new_point = [new_point_h, new_point_w]
-            value = double_list_get_list(map_list, new_point)
-            breaking_brick(new_point)
-            print(value_index)
-        removed_list.append(temp_removed_list)
-
-    first_shout_result = first_shout()
-    if first_shout_result == False:
-        return
-    elif first_shout_result == True:
-        return True
+        for temp_cell in temp_cell_list:
+            cell.append(temp_cell)
 
 
 test_case_num = int(input())
-
-
-def find_top_level(w_pram):
-    global map_list
-    for _h_index in range(h):
-        if map_list[_h_index][w_pram] != 0:
-            return _h_index
-    return -1
-
-
-def first_shout():
-    global n
-    n -= 1
-    if n == 0:
-        return False
-    for _w_index in range(w):
-        h_index = find_top_level(_w_index)
-        if h_index != -1:
-            breaking_brick([h_index, _w_index])
-    return True  # 벽돌을 다 깬 경우
-
-
 for test_case_index in range(test_case_num):
-    n, w, h = map(int, input().split())
+    n, m, k = map(int, input().split())
+    count = 0
     # 입력의 첫 번째 줄은 배열의 행 수입니다.
-    map_list = []
     result = 0
-    for i in range(h):
-        temp_list = list(map(int, input().split()))
-        for temp in temp_list:
-            if temp != 0:
-                result += 1
-        map_list.append(temp_list)
-    min_result = result
-    first_shout()
+    map_list = []
+    for i in range(2 * k + n):
+        map_list.append([0] * (2 * k + m))
+    cell_list = []
 
-    print("#%d %d" % (test_case_index + 1, min_result))
+    for i in range(n):
+        temp_list = list(map(int, input().split()))
+        for temp_index in range(len(temp_list)):
+            if temp_list[temp_index] != 0:
+                result += 1
+                try:
+                    cell_list[temp_list[temp_index]].append([
+                        (k + i, k + temp_index),
+                        temp_list[temp_index]
+                    ])
+                except IndexError:
+                    for _i in range(len(cell_list), temp_list[temp_index] + 1):
+                        cell_list.append([])
+                    cell_list[temp_list[temp_index]].append([
+                        (k + i, k + temp_index),
+                        temp_list[temp_index]
+                    ])
+            map_list[k + i][k + temp_index] = temp_list[temp_index]
+    for k_index in range(k):
+        print(k_index)
+        for _map in map_list:
+            print(_map)
+        grow_cells()
+    print("#%d %d" % (test_case_index + 1, result))
