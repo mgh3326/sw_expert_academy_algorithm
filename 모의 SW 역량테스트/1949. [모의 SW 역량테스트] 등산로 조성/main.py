4d5edd9f6acbe5368e03304341b6cb74174effe5
kwanghyunMoon <mgh3326@naver.com>
2019-08-14 10:22:58 +0900

    update

--- 모의 SW 역량테스트/1949. [모의 SW 역량테스트] 등산로 조성/main.py
+++ 모의 SW 역량테스트/1949. [모의 SW 역량테스트] 등산로 조성/main.py
@@ -1,6 +1,5 @@
-def add(a, b):
-    return int(a) + int(b) + 1
-
+def dfs():
+    pass
 
 def solution():
     test_case_num = int(input())
@@ -8,12 +7,18 @@ def solution():
         n, k = map(int, input().split())
         # 입력의 첫 번째 줄은 배열의 행 수입니다.
         map_list = []
+        max_value = 0
         for i in range(n):
-            map_list.extend(list(map(int, input().split())))
+            temp_list = list(map(int, input().split()))
+            temp_max_value = max(temp_list)
+            if temp_max_value > max_value:
+                max_value = temp_max_value
+            for i in range(len(temp_list)):
+                temp_list[i] = temp_list[i], False
+            map_list.append(temp_list)
         max_num = max(map_list)
         max_num_index_list = [i for i, val in enumerate(map_list) if val == max_num]
         # 와 코드를 이렇게도 짤수 있구나 if 저게 True면 i를 반환하나보다 멋지다리
-        
 
         print("t")
 
---
+++ 모의 SW 역량테스트/5658. [모의 SW 역량테스트] 보물상자 비밀번호/main.py
@@ -0,0 +1,31 @@
+test_case_num = int(input())
+
+for test_case_index in range(test_case_num):
+    n, k = map(int, input().split())
+    result = 0
+    s = input()
+    my_list = []
+    count = 0
+    split_size = n // 4
+    my_dict = {}
+
+    while True:
+
+        if count == n:
+            break
+        my_list.append(s[count: count + split_size])
+        count += split_size
+    for i in range(split_size):
+        for my in my_list:
+            if my not in my_dict:
+                my_dict[my] = True
+        temp_list = []
+        for my_list_index in range(len(my_list)):
+            temp_list.append(my_list[my_list_index - 1][-1] + my_list[my_list_index][:split_size - 1])
+        my_list = temp_list
+    l = sorted(my_dict.keys(), reverse=True)
+    # for _l in l:
+
+    k_ = int(l[k-1], 16)
+
+    print("#" + str(test_case_index + 1), k_)
