3187de9e8ef445d31ecce52c9e2d2a0873752d60
kwanghyunMoon <mgh3326@naver.com>
2019-10-19 00:04:44 +0900

    완탐 no visit list

--- 모의 SW 역량테스트/5650. [모의 SW 역량테스트] 핀볼 게임/다시풀기.py
+++ 모의 SW 역량테스트/5650. [모의 SW 역량테스트] 핀볼 게임/다시풀기.py
@@ -16,95 +16,48 @@ dir_list = [
     [0, 1],  # 우
 ]
 
-
-def dfs(input_h, input_w, input_dir_idx, depth):
-    global current_score
-    global result
-    if depth != 0 and (
-            (input_h == start_h and input_w == start_w) or board_value_list[input_h][input_w] == -1):  # 종료
-        if result < current_score:
-            result = current_score
-        return
-    dh, dw = dir_list[input_dir_idx]
-    nh = input_h + dh
-    nw = input_w + dw
-    if nh < 0 or nw < 0 or nh >= N or nw >= N:
-        current_score += 1
-        if board_value_list[input_h][input_w] == 0:
-            if visit_list[input_h][input_w][block_dict[5][input_dir_idx]] >= current_score:
-                return
-            visit_list[input_h][input_w][block_dict[5][input_dir_idx]] = current_score
-        if 1 <= board_value_list[input_h][input_w] <= 5:
-            current_score += 1
-
-            dfs(input_h, input_w, block_dict[board_value_list[input_h][input_w]][block_dict[5][input_dir_idx]],
-                depth + 1)
-        elif 5 < board_value_list[input_h][input_w]:  # 웜홀 일때
-            warm_hall_h, warm_hall_w = warm_hall_dict[input_h, input_w]
-            dfs(warm_hall_h, warm_hall_w, block_dict[5][input_dir_idx], depth + 1)
-        else:
-            dfs(input_h, input_w, block_dict[5][input_dir_idx], depth + 1)
-        return
-    elif 1 <= board_value_list[nh][nw] <= 5:
-        current_score += 1
-        # if board_value_list[input_h][input_w] == 0:
-        #     if visit_list[input_h][input_w][block_dict[board_value_list[nh][nw]][input_dir_idx]] >= current_score:
-        #         return
-        #     visit_list[input_h][input_w][block_dict[board_value_list[nh][nw]][input_dir_idx]] = current_score
-        dfs(nh, nw, block_dict[board_value_list[nh][nw]][input_dir_idx], depth + 1)
-        return
-    elif 5 < board_value_list[nh][nw]:  # 웜홀 일때
-        warm_hall_h, warm_hall_w = warm_hall_dict[nh, nw]
-        dfs(warm_hall_h, warm_hall_w, input_dir_idx, depth + 1)
-        return
-    elif (nh == start_h and nw == start_w) or board_value_list[nh][nw] == -1:  # 종료
-        if result < current_score:
-            result = current_score
-        return
-    elif board_value_list[nh][nw] == 0:
-        # if visit_list[nh][nw][input_dir_idx] >= current_score:
-        #     return
-        # visit_list[nh][nw][input_dir_idx] = current_score
-        dfs(nh, nw, input_dir_idx, depth + 1)
-    return
-
-
 test_case_num = int(input())
 for test_case_index in range(test_case_num):
     result = 0
-    board_value_list = []
-    visit_list = []
-    warm_hall_dict = {}
     N = int(input())
+    board_list = [[5] * (N + 2)]
+    warm_hall_dict = {}
     for h in range(N):
-        temp_list = []
-        temp_list.append(5)
-        temp_list = list(map(int, input().split()))
-        temp_visit_list = []
-        for w in range(len(temp_list)):
-            visit_value = []
-            temp_value = temp_list[w]
-            if temp_value == 0:
-                visit_value = [-1] * len(dir_list)
-            elif 6 <= temp_value <= 10:
-                if temp_value in warm_hall_dict:
-                    warm_hall_dict[h, w] = warm_hall_dict[temp_value]
-                    warm_hall_dict[warm_hall_dict[temp_value]] = (h, w)
-                    warm_hall_dict.pop(temp_value)
+        temp_list = [5]
+        temp_list.extend(list(map(int, input().split())))
+        for w in range(1, len(temp_list)):
+            if temp_list[w] > 5:
+                if temp_list[w] not in warm_hall_dict:
+                    warm_hall_dict[temp_list[w]] = (h + 1, w)
                 else:
-                    warm_hall_dict[temp_value] = (h, w)
-            temp_visit_list.append(visit_value)
-        visit_list.append(temp_visit_list)
-        board_value_list.append(temp_list)
-    for start_h in range(N):
-        for start_w in range(N):
-            if board_value_list[start_h][start_w] == 0:
-                for dir_idx in range(len(dir_list)):
-                    current_score = 0
-                    if visit_list[start_h][start_w][dir_idx] >= current_score:
-                        continue
-                    visit_list[start_h][start_w][dir_idx] = current_score
+                    warm_hall_dict[h + 1, w] = warm_hall_dict[temp_list[w]]
+                    warm_hall_dict[warm_hall_dict[temp_list[w]]] = (h + 1, w)
+                    warm_hall_dict.pop(temp_list[w])
 
-                    dfs(start_h, start_w, dir_idx, 0)
+        temp_list.append(5)
+        board_list.append(temp_list)
+    board_list.append([5] * (N + 2))
+    for start_h in range(1, N + 1):
+        for start_w in range(1, N + 1):
+            if board_list[start_h][start_w] != 0:
+                continue
+            for current_dir in range(4):
+                current_h, current_w = start_h, start_w
+                score = 0
+                count = 0
+                while True:
+                    if (count != 0 and start_h == current_h and start_w == current_w) or board_list[current_h][
+                        current_w] == -1:  # 시작점으로 와버림 또는 블랙홀
+                        break
+                    if 1 <= board_list[current_h][current_w] <= 5:
+                        current_dir = block_dict[board_list[current_h][current_w]][current_dir]
+                        score += 1
+                    elif board_list[current_h][current_w] > 5:
+                        (current_h, current_w) = warm_hall_dict[current_h, current_w]
+                    dh, dw = dir_list[current_dir]
+                    current_h, current_w = dh + current_h, dw + current_w
+                    count += 1
+                if score > result:
+                    result = score
 
     print("#%d %d" % (test_case_index + 1, result))
